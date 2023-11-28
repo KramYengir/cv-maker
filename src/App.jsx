69ebe7865e0d124ae327973620324698ee3e64a3
KramYengir <kramyengir@gmail.com>
2023-11-28 15:14:28 +0000

    begin initial layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import "./App.css";
+import "./styles/App.css";
+import Header from "./components/Header";
+import InputSection from "./components/InputSection";
+import Preview from "./components/Preview";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <main>
+        <InputSection></InputSection>
+        <Preview></Preview>
+      </main>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import "../styles/Header.css";
+
+const Header = () => {
+  return (
+    <header className="container">
+      <h3>CV Maker+</h3>
+    </header>
+  );
+};
+
+export default Header;
